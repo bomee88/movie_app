c23bfe7a589b0616a0e0b16ccdd673d13b81e4c8
bomee88 <36142662+bomee88@users.noreply.github.com>
2020-01-11 22:08:27 +0900

    react practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count : 0
+  }
+  plus = ()=>{
+    this.setState(current=>({count: current.count + 1}));
+  }
+  minus = ()=>{
+    this.setState(current=>({count: current.count - 1}));
+  }
+  compoen
+  render(){
+    return (
+      <div>
+        <h1>this is numner : {this.state.count}</h1>
+        <button onClick={this.plus}>+</button>
+        <button onClick={this.minus}>-</button>         
+      </div>
+    );
+  }
+/*
+  react의 life cycle method (component의 생성과 삭제)
+  1. Mounting = 태어남
+   - constructor() : javascript기반, 처음 웹에 들어갈때
+ 
+  2. Updating = 업데이트 됨
+   - render() : 사용자가 어떤 액션을 해서 화면이 랜더링 되는 중일때
+   - componentDidUpdate() : 랜더링 되고 난 뒤(업데이트 되고 난 뒤) 
+
+  3. Unmounting = 죽음
+   - componentWillUnmount() : 화면을 나갈때, 화면이 죽을때.
+*/
 }
 
 export default App;
